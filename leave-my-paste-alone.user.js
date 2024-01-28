b2a236909f8b8b521898530cbda0d52919905a8f
LinuxPony <sonic.ert@gmail.com>
2024-01-28 00:17:09 -0800

    Create leave-my-paste-alone.user.js

---
+++ leave-my-paste-alone.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Leave my paste alone!
+// @namespace    https://github.com/sonicer105
+// @description  Prevents site from disallowing paste. I know what I'm doing damn it!
+// @author       LinuxPony
+// @version      2024-01-28
+// @license      MIT
+// @updateURL    https://raw.githubusercontent.com/sonicer105/leave-my-paste-alone/master/leave-my-paste-alone.user.js
+// @downloadURL  https://raw.githubusercontent.com/sonicer105/leave-my-paste-alone/master/leave-my-paste-alone.user.js
+// @match        http://*/*
+// @match        https://*/*
+// @run-at       document-start
+// @grant        none
+// @connect      *
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.addEventListener(
+        "paste",
+        function(e) {
+            e.stopImmediatePropagation()
+        },
+        true
+    );
+})();
